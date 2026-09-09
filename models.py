90e7cdffe748ecdf967b7c8f93d30ecd2c968f11
Ivan <ivanperlov626@gmail.com>
2026-09-09 10:40:22 +0300

    Work with tasks comleted

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, field_validator, AwareDatetime
 from typing import Optional
 from enum import Enum
 import re
+from datetime import datetime
 
 class UserRole(str, Enum):
     ADMIN = 'admin'
-    USER = 'user'
-    MANAGER = 'manager'
+    MEMBER = 'member'
+    MODERATOR = 'moderator'
 
 class UserCreate(BaseModel):
     username: str = Field(max_length=64)
@@ -25,24 +26,44 @@ class UserCreate(BaseModel):
         return value
 
 class UserInDB(UserCreate):
-    role: UserRole = UserRole.USER
+    is_admin: bool = False
 
 class User(BaseModel):
     id: int
     username: str
     password: str
-    role: UserRole
+    is_admin: bool
 
-class TodoCreate(BaseModel):
+class TeamCreate(BaseModel):
+    name: str = Field(min_length=2, max_length=24)
+    description: str = Field(max_length=1200)
+
+class Team(BaseModel):
+    id: int
     owner_id: int
+    name: str
+    description: str
+    members_count: int
+
+class TaskCreate(BaseModel):
     title: str = Field(..., max_length=24)
     description: str = Field(max_length=1500)
+    deadline: Optional[AwareDatetime] = None
+    is_personal: bool
+    team_id: Optional[int] = None
 
-class TodoUpdate(BaseModel):
-    id: int
-    title: Optional[str] = Field(max_length=24)
-    description: Optional[str] = Field(max_length=1500)
-    completed: bool = False
+class TaskInDB(BaseModel):
+    owner_id: int
+    team_id: Optional[int] = None
+    title: str
+    description: str
+    deadline: Optional[AwareDatetime] = None
+    is_personal: bool
+
+class TaskUpdate(BaseModel):
+    description: Optional[str] = None
+    completed: bool
+    version: int
 
 class TodoBulkUpdate(BaseModel):
     ids: list[int]
